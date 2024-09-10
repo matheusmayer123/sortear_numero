155162380e440142b7d98979ab5d02cb9756c41a
Matheus Mayer <118402543+matheusmayer123@users.noreply.github.com>
2024-09-10 11:41:29 -0300

    v1.0

---
+++ app.js
@@ -0,0 +1,55 @@
+let lista_numeros_sorteados = [];
+
+
+function getRndIntegerMinMax(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    let numeroSorteado = Math.floor(Math.random() * (max - min)) + min;
+
+    return numeroSorteado;
+}
+
+
+function sortear() {
+
+    let numDigitado = document.getElementById('quantidade').value;
+
+    let minimo = document.getElementById('de').value;
+
+    let maximo = document.getElementById('ate').value;
+
+
+    for (let i = 0; i < numDigitado; i++) {
+
+        lista_numeros_sorteados.push(getRndIntegerMinMax(minimo, maximo));
+
+    }
+
+    exibirResultado('h3', `Os números sorteados foram: ${lista_numeros_sorteados}`);
+    document.getElementById('btn-sortear').setAttribute('disabled', true)
+    document.getElementById('btn-reiniciar').removeAttribute('disabled');
+
+
+}
+
+function exibirResultado(tag, texto) {
+
+    campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+
+function reiniciar() {
+
+    let campo1 = document.getElementById('quantidade');
+    let campo2 = document.getElementById('de');
+    let campo3 = document.getElementById('ate');
+    campo1.value = '';
+    campo2.value = '';
+    campo3.value = '';
+    lista_numeros_sorteados = [];
+    document.getElementById('btn-sortear').removeAttribute('disabled');
+    document.getElementById('btn-reiniciar').setAttribute('disabled', true)
+
+}
